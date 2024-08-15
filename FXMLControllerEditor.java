cd199de53aa15a8368c9c96bb414c6f5ff2bfbc0
Joanne Bahrs <139918856+projo70@users.noreply.github.com>
2024-08-15 10:49:36 +0200

    Update FXMLControllerEditor.java

--- FXMLControllerEditor.java
+++ FXMLControllerEditor.java
@@ -1,4 +1,4 @@
-package java12e;
+package editor;
 
 import java.io.File;
 import java.io.FileReader;
@@ -19,38 +19,37 @@ import javafx.stage.FileChooser.ExtensionFilter;
 public class FXMLControllerEditor {
 	//das Textfeld
 		@FXML private TextArea editor;
-		//für die Bühne
+		//die Buehne
 		private Stage meineStage;
 		
-		//Methode zum Anzeigen des Öffnendialog
+		//Methode zum Anzeigen des Oeffnendialog
 		@FXML protected void ladenKlick(ActionEvent event) {
 			//eine neue Instanz der Klasse FileChooser erzeugen
 			FileChooser oeffnenDialog = new FileChooser();
-			//den Titel für den Dialog setzen
+			//den Titel fuer den Dialog setzen
 			oeffnenDialog.setTitle("Datei oeffnen");
 			oeffnenDialog.setInitialDirectory(new File(System.getProperty("user.home")));
-			//den Filter setzen
+			//Filter setzen
 			oeffnenDialog.getExtensionFilters().add(new ExtensionFilter("Textdateien","*.txt"));
-			//den Öffnendialog anzeigen und das Ergebnis beschaffen
+			//den Oeffnendialog anzeigen und das Ergebnis beschaffen
 			File datei = oeffnenDialog.showOpenDialog(meineStage);
-			//wurde eine Datei ausgewählt
+			//wurde eine Datei ausgewaehlt
 			if (datei != null)
-				//dann den Inhalt über eine eigene Methode lesen
+				//Inhalt ueber eine eigene Methode lesen
 				editor.setText(datenLesen(datei));
 		}
 
-		//die Methode zum Laden
-		//Sie erhält die Datei, die gelesen werden soll
-		//und liefert eine Zeichenkette mit dem Inhalt zurück
+		//Methode zum Laden
+		//enthaehlt die Datei, die gelesen werden soll und liefert eine Zeichenkette mit dem Inhalt zurueck
 		private String datenLesen(File dateiLesen) {
-			//gelesen wird als int!!
+			//gelesen wird als int
 			int zeichen = 0;
 
 			StringBuilder text = new StringBuilder();
 			//eine Instanz der Klasse FileReader mit der Datei erzeugen
 			try (FileReader tempDatei = new FileReader(dateiLesen)){
 				//solange das Ende der Datei nicht erreicht ist, werden die Zeichen einzeln gelesen und
-				//an einem StringBuilder angehängt
+				//an einem StringBuilder angehaengt
 				zeichen = tempDatei.read();
 				while (zeichen != -1) {
 					//zeichen wird in den Typ char umgewandelt
@@ -67,28 +66,28 @@ public class FXMLControllerEditor {
 			return (text.toString());
 		}
 		
-		//die Methode zum Anzeigen des Speicherndialogs
+		//Methode zum Anzeigen des Speicherndialogs
 		@FXML protected void speichernKlick(ActionEvent event) {
 			//eine neue Instanz der Klasse FileChooser erzeugen
 			FileChooser speichernDialog = new FileChooser();
-			//den Titel für den Dialog setzen
+			//den Titel fÃ¼r den Dialog setzen
 			speichernDialog.setTitle("Datei speichern");
 			
-			//den Ordner setzen
+			//Ordner setzen
 			speichernDialog.setInitialDirectory(new File(System.getProperty("user.home")));
 			
-			//den Filter setzen
+			//Filter setzen
 			speichernDialog.getExtensionFilters().add(new ExtensionFilter("Textdateien", "*.txt"));
-			//den Speicherndialog anzeigen und das Ergebnis beschaffen
+			//Speicherndialog anzeigen und Ergebnis beschaffen
 			File datei = speichernDialog.showSaveDialog(meineStage);
-			//wurde eine Datei ausgewählt
+			//wurde eine Datei ausgewaehlt
 			if (datei != null)
-				//dann den Inhalt über eine eigene Methode speichern
+				//Inhalt ueber eine eigene Methode speichern
 				datenSchreiben(datei);
 		}
 
-		//die Methode zum Schreiben
-		//Sie erhält die Datei, die geschrieben werden soll
+		//Methode zum Schreiben
+		//erhaelt die Datei, die geschrieben werden soll
 		private void datenSchreiben(File dateiSchreiben) {
 			//eine Instanz der Klasse FileWriter mit der Datei erzeugen
 			try (FileWriter tempDatei = new FileWriter(dateiSchreiben)){
@@ -97,25 +96,21 @@ public class FXMLControllerEditor {
 			}
 			catch (IOException e) {
 				//bei Problemen einen Dialog erzeugen
-				//bei Problemen einen Dialog erzeugen
 				Alert meinDialog = new Alert(AlertType.INFORMATION, "Beim Schreiben ist ein Problem aufgetreten");
 				meinDialog.setHeaderText("Bitte beachten");
 				meinDialog.showAndWait();
 			}
 		}
-		
+
+		//Methode zur Erstellung eines neuen Dokuments
 		@FXML protected void neuKlick(ActionEvent event) {
-			//Aufgabe 2:
-			//Wenn der Anwender auf new klickt, wird er gefragt ob er ein neues Dokument erstellen möchte.
-			//Klickt er auf nein, passiert nichts. 
-			//Klickt der Anwender auf ja, wird er gefragt ob er das aktuelle Dokument vorher speichern möchte.
-			//Dann öffnet sich der Speicherndialog bevor ein neues Dokument erstellt wird
-			//bei nein wird direkt ein neues Dokument erstellt.
+			//Abfrage zur Erstellung eines neuen Dokuments
 			Alert meinDialog = new Alert(AlertType.CONFIRMATION,"Die Daten loeschen und ein neues Dokment erstellen?",ButtonType.YES,ButtonType.NO);
 			meinDialog.setTitle("Neues Dokument erstellen");
 			meinDialog.setHeaderText("bitte bestaetigen");
 			meinDialog.showAndWait();
 			if (meinDialog.getResult()==ButtonType.YES) {
+				//Abfrage ob Datei vorher gespeichert werden soll
 				Alert vorherSpeichern = new Alert(Alert.AlertType.CONFIRMATION,"Wollen Sie die Daten speichern?",ButtonType.YES,ButtonType.NO);
 				vorherSpeichern.setTitle("Frage");
 				vorherSpeichern.setHeaderText("bitte auswaehlen");
@@ -128,19 +123,15 @@ public class FXMLControllerEditor {
 					editor.clear();
 			}		
 		}
-		
+
+		//Methode zum Beenden des Programms
 		@FXML protected void beendenKlick(ActionEvent event) {
-			//Aufgabe 2:
-			//Wenn der Anwender auf quit klickt, wird er gefragt ob er das Porgramm beenden möchte.
-			//Klickt er auf nein, passiert nichts. 
-			//Klickt er auf ja, wird der der Anwender gefragt ob er nochmal speichern möchte.
-			//Klickt der Anwender auf ja, wird der Speicherndialog geöffnet bevor das Programm beendet wird. 
-			//Klickt der Anwender auf nein, wird das Programm beendet.
 			Alert beendenDialog = new Alert(AlertType.CONFIRMATION,"Wollen Sie das Programm schliessen?",ButtonType.YES,ButtonType.NO);
 			beendenDialog.setTitle("Programm schliessen");
 			beendenDialog.setHeaderText("bitte bestaetigen");
 			beendenDialog.showAndWait();
 			if (beendenDialog.getResult() == ButtonType.YES) {
+				//Abfrage Datei vor dem Schliessen speichern
 				Alert vorherSpeichern = new Alert(Alert.AlertType.CONFIRMATION,"Wollen Sie die Daten speichern?",ButtonType.YES,ButtonType.NO);
 				vorherSpeichern.setTitle("Frage");
 				vorherSpeichern.setHeaderText("bitte auswaehlen");
@@ -157,15 +148,15 @@ public class FXMLControllerEditor {
 		public void setMeineStage(Stage meineStage) {
 			this.meineStage = meineStage;
 		}
-		//Aufgabe 3: das ActionEvent für Ausschneiden.
+		//ActionEvent fuer Ausschneiden
 		@FXML protected void cut(ActionEvent event) {
 			editor.cut();
 		}
-		//Aufgabe 3: das ActionEvent für Kopieren.
+		//ActionEvent fuer Kopieren
 		@FXML protected void copy(ActionEvent event) {
 			editor.copy();
 		}
-		//Aufgabe 3: das ActionEvent für Einfügen.
+		//ActionEvent fuer Einfuegen
 		@FXML protected void paste(ActionEvent event) {
 			editor.paste();
 		}
